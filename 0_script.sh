cd49deaabe97ff8edae101c7d59cacc949f0ec68
Guy Barros <guy@Hashicorp.com>
2023-04-25 01:17:06 +0100

    dc2 services

--- 0_script.sh
+++ 0_script.sh
@@ -6,7 +6,7 @@ aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terr
 aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name_cluster2)
 #AP2
 aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name_cluster3)
-#DC1
+#DC2
 aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name_cluster4)
 #AP3
 aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name_cluster5)
@@ -16,11 +16,11 @@ aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terr
 kubectl config get-clusters
 
 # update these variables every deploy
-export dc1=arn:aws:eks:eu-west-2:958215610051:cluster/cluster1-eks-FCULu3wZ
-export ap1=arn:aws:eks:eu-west-2:958215610051:cluster/cluster2-eks-uYpNor5H
-export ap2=arn:aws:eks:eu-west-2:958215610051:cluster/cluster3-eks-74VK3cXk
-export dc2=arn:aws:eks:eu-west-2:958215610051:cluster/cluster4-eks-qH5KiyGj
-export ap3=arn:aws:eks:eu-west-2:958215610051:cluster/cluster5-eks-Ob5nbEdb
+export dc1=arn:aws:eks:eu-west-2:958215610051:cluster/cluster1-eks-GVXQiaCh
+export ap1=arn:aws:eks:eu-west-2:958215610051:cluster/cluster2-eks-2b2KUDB2
+export ap2=arn:aws:eks:eu-west-2:958215610051:cluster/cluster3-eks-YZTNKrQs
+export dc2=arn:aws:eks:eu-west-2:958215610051:cluster/cluster4-eks-vwSLaBw6
+export ap3=arn:aws:eks:eu-west-2:958215610051:cluster/cluster5-eks-XeBKytxF
 
 # DC1
 kubectl config use-context $dc1
--- 1_deploy.sh
+++ 1_deploy.sh
@@ -18,6 +18,8 @@ kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/
 # Deploy service-b to tenant-2
 kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-b-v1.yaml --context $ap2
 kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-b-v2.yaml --context $ap2
+# Deploy service-a to tenant-2
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-a.yaml --context $ap2
 
 #Add intention
 kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/intentions.yaml --context $ap2 
@@ -37,10 +39,48 @@ kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/
 kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/igw.yaml --context $ap1
 
 
+kubectl get svc -n consul --context $ap1
+
+######################################## Peering #########################################
+#################### Services -Default DC2 ##################
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/dc2_default/service-a.yaml --context $dc2
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/dc2_default/service-b-v1.yaml --context $dc2
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/dc2_default/service-c-v1.yaml --context $dc2
+
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/dc2_default/igw.yaml --context $dc2
+
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/dc2_default/intentions.yaml --context $dc2
+#################### Peer dc1-tentant-2 to dc2-default ##################
+#create an acceptor in tenant-2
+kubectl apply -f  /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/peering/acceptor-on-ap2-for-dc2.yaml --context $ap2
+#check if CRD got created
+kubectl get peeringacceptors --context $ap2
+#create peering token secret from DC1 in DC2 (peering token has the same name as the acceptor created above)
+kubectl get secret peering-token-dc2-default --context $ap2 -o yaml | kubectl apply --context $dc2 -f -
+#Create dialer from dc2 default to dc1 tenant-2 
+kubectl apply -f  /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/peering/dialer-dc2.yaml --context $dc2
+#export services from dc2 to dc1 
+kubectl apply -f  /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/peering/exported.yaml --context $dc2
 
 
-#################### TGW ##################
 
+
+#################### Traffic Management ##################
+
+# kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-b-router-header.yaml --context $ap2
+# kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-b-router-query.yaml --context $ap2
+
+# kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-a-router-header.yaml --context $ap1
+#################### Router ##################
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-a-router-query.yaml --context $ap1
+#################### Splitter ##################
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-2/service-b-splitter.yaml --context $ap2
+
+#################### Resolver ##################
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-a-resolver.yaml --context $ap1
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-a-resolver.yaml --context $ap2
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-b-resolver.yaml --context $ap2
+kubectl apply -f /Users/guybarros/GIT_ROOT/terraform-consul-ap-peer/deployments/tenant-1/service-c-resolver.yaml --context $ap2
 
 ######################################     Testing ###############################
 
